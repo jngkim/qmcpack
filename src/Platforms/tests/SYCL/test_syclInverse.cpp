06406bfb9320f094ed8299fc2881691f9469ab39
Jeongnim Kim <jeongnim.kim@gmail.com>
2021-11-13 08:21:10 -0800

    Replace MKL RNG with std RNG.

--- src/Platforms/tests/SYCL/test_syclInverse.cpp
+++ src/Platforms/tests/SYCL/test_syclInverse.cpp
@@ -14,13 +14,13 @@
 #include <memory>
 #include <vector>
 #include <iostream>
+#include <random>
 #include "OMPTarget/OMPallocator.hpp"
 #include "SYCL/SYCLruntime.hpp"
 #include "SYCL/SYCLallocator.hpp"
 #include <OhmmsPETE/OhmmsVector.h>
 #include <OhmmsPETE/OhmmsMatrix.h>
 #include <CPU/BLAS.hpp>
-#include "oneapi/mkl/rng.hpp"
 #include "oneapi/mkl/lapack.hpp"
 #include "mkl.h"
 
@@ -31,37 +31,36 @@ namespace qmcplusplus
 namespace syclSolver=oneapi::mkl::lapack;
 
 
-#if 0
 template<typename T, typename Alloc>
 void test_inverse(const std::int64_t M)
 {
-  using mat_t = Matrix<T, Alloc>;
-  using vec_t = Vector<T, Alloc>;
+  sycl::queue handle{*get_default_queue()};
 
-  sycl::queue *handle=get_default_queue();
+  Matrix<T,Alloc> A(M,M);
+  Matrix<T>       B(M,M);
 
-  mat_t A(M,M);
-  mat_t B(M,M);
+  { //intialize B on host with random numbers
+    std::mt19937 rng;
+    std::uniform_real_distribution<T> udist{T(-0.5),T(0.5)}; 
+    std::generate_n(B.data(),B.size(),[&]() { return udist(rng);});
+    handle.memcpy(A.device_data(),B.data(), B.size()*sizeof(T)).wait();
+  }
 
-  oneapi::mkl::rng::mt19937 rng{*handle};
-  oneapi::mkl::rng::uniform<T> udist{T(-0.5),T(0.5)}; 
-
-  oneapi::mkl::rng::generate(udist, rng, A.size(), A.device_data()).wait(); 
-  A.updateFrom();
-  std::copy(A.data(),A.data()+A.size(), B.data()); //save B
-
-  auto getrf_ws=syclSolver::getrf_scratchpad_size<T>(*handle,M,M,M);
-  auto getri_ws=syclSolver::getri_scratchpad_size<T>(*handle,M,M);
-
+  //allocate pivots and workspaces on the device
+  auto getrf_ws=syclSolver::getrf_scratchpad_size<T>(handle,M,M,M);
+  auto getri_ws=syclSolver::getri_scratchpad_size<T>(handle,M,M);
   Vector<std::int64_t,SYCLAllocator<std::int64_t>> pivots(M);
   Vector<T,SYCLAllocator<T>> workspace(std::max(getrf_ws,getri_ws));
 
-  auto e = syclSolver::getrf(*handle,M,M,A.device_data(),M, pivots.data(), workspace.data(), getrf_ws);
-  syclSolver::getri(*handle,M,A.device_data(),M,pivots.data(), workspace.data(), getri_ws, {e}).wait();
+  //getrf (LU) -> getri (inverse)
+  auto e = syclSolver::getrf(handle,M,M,A.device_data(),M, pivots.data(), workspace.data(), getrf_ws);
+  syclSolver::getri(handle,M,A.device_data(),M,pivots.data(), workspace.data(), getri_ws, {e}).wait();
+
+  //update A on host for comparison
   A.updateFrom();
 
   //check the identity
-  mat_t C(M,M);
+  Matrix<T> C(M,M);
   BLAS::gemm('N', 'N', M, M, M, 1.0, B.data(), M, A.data(), M, 0.0, C.data(),M);
   for(int i=0; i<M; ++i)
   {
@@ -88,9 +87,7 @@ TEST_CASE("OmpSYCL inverse", "[SYCL]")
 #endif
 
 }
-#endif
 
-#if 1
 template<typename T, typename Alloc>
 void test_inverse_batched(const std::int64_t M, int batch_count)
 {
@@ -99,12 +96,17 @@ void test_inverse_batched(const std::int64_t M, int batch_count)
   using pvec_t = Vector<std::int64_t, SYCLAllocator<std::int64_t>>;
   using dvec_t = Vector<T, SYCLAllocator<T>>;
 
-  sycl::queue *handle=get_default_queue();
+  sycl::queue handle{*get_default_queue()};
 
   std::vector<mat_t> As;
   As.resize(batch_count);
 
-  mat_t B(M,M);
+  Matrix<T> B(M,M);
+  {
+    std::mt19937 rng;
+    std::uniform_real_distribution<T> udist{T(-0.5),T(0.5)}; 
+    std::generate_n(B.data(),B.size(),[&]() { return udist(rng);});
+  }
 
   //pivot vectors
   std::vector<pvec_t> Ps;
@@ -114,20 +116,14 @@ void test_inverse_batched(const std::int64_t M, int batch_count)
   std::vector<dvec_t> Ws;
   Ws.resize(batch_count);
 
-  oneapi::mkl::rng::mt19937 rng{*handle};
-  oneapi::mkl::rng::uniform<T> udist{T(-0.5),T(0.5)}; 
-
-  auto getrf_ws=syclSolver::getrf_scratchpad_size<T>(*handle,M,M,M);
-  auto getri_ws=syclSolver::getri_scratchpad_size<T>(*handle,M,M);
-  auto ws=std::max(getrf_ws,getri_ws);
-
-  oneapi::mkl::rng::generate(udist, rng, B.size(), B.device_data()).wait(); 
-  B.updateFrom(); 
+  auto getrf_ws=syclSolver::getrf_scratchpad_size<T>(handle,M,M,M);
+  auto getri_ws=syclSolver::getri_scratchpad_size<T>(handle,M,M);
+  auto ws=std::max(getrf_ws,getri_ws); 
 
   for (int batch = 0; batch < batch_count; batch++)
   {
     As[batch].resize(M,M);
-    handle->memcpy(As[batch].device_data(),B.device_data(), B.size()*sizeof(T)).wait();
+    handle.memcpy(As[batch].device_data(),B.data(), B.size()*sizeof(T)).wait();
     Ps[batch].resize(M);
     Ws[batch].resize(ws);
   }
@@ -135,12 +131,12 @@ void test_inverse_batched(const std::int64_t M, int batch_count)
   std::vector<sycl::event> lu_events(batch_count);
   for (int batch = 0; batch < batch_count; batch++)
   {
-    lu_events[batch] = syclSolver::getrf(*handle,M,M,As[batch].device_data(),M,
+    lu_events[batch] = syclSolver::getrf(handle,M,M,As[batch].device_data(),M,
         Ps[batch].data(), Ws[batch].data(), getrf_ws);
   }
   for (int batch = 0; batch < batch_count; batch++)
   {
-    lu_events[batch] = syclSolver::getri(*handle,M,As[batch].device_data(),M,
+    lu_events[batch] = syclSolver::getri(handle,M,As[batch].device_data(),M,
         Ps[batch].data(), Ws[batch].data(), getri_ws,{lu_events[batch]});
   }
 
@@ -179,5 +175,5 @@ TEST_CASE("OmpSYCL batch-inverse", "[SYCL]")
 #endif
 
 }
-#endif
+
 } // namespace qmcplusplus
