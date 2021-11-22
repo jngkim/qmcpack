2dad9907f82d04a2266e3e9eba3a9a2cb3d9719c
Jeongnim Kim <jeongnim.kim@gmail.com>
2021-11-22 13:46:50 -0800

    Add a test using SYCLHostAllocator to manage pointers

--- src/Platforms/tests/SYCL/test_syclBLAS.cpp
+++ src/Platforms/tests/SYCL/test_syclBLAS.cpp
@@ -177,6 +177,99 @@ void test_gemv_batched(const int M_b, const int N_b, const char trans, const int
   }
 }
 
+template<typename T>
+void test_gemv_batched_pinned(const int M_b, const int N_b, const char trans, const int batch_count)
+{
+  const int M = trans == 'T' ? M_b : N_b;
+  const int N = trans == 'T' ? N_b : M_b;
+
+  using vec_t = Vector<T, OMPallocator<T>>;
+  using mat_t = Matrix<T, OMPallocator<T>>;
+
+  sycl::queue *handle=get_default_queue();
+
+  // Create input vector
+  std::vector<vec_t> As;
+  Vector<const T*, SYCLHostAllocator<const T*>> Aptrs;
+
+  // Create input matrix
+  std::vector<mat_t> Bs;
+  Vector<const T*, SYCLHostAllocator<const T*>> Bptrs;
+
+  // Create output vector (ompBLAS)
+  std::vector<vec_t> Cs;
+  Vector<T*, SYCLHostAllocator<T*>> Cptrs;
+
+  // Create output vector (BLAS)
+  std::vector<vec_t> Ds;
+  Vector<T*, SYCLHostAllocator<T*>> Dptrs;
+
+  // Resize pointer vectors
+  Aptrs.resize(batch_count);
+  Bptrs.resize(batch_count);
+  Cptrs.resize(batch_count);
+  Dptrs.resize(batch_count);
+
+  // Resize data vectors
+  As.resize(batch_count);
+  Bs.resize(batch_count);
+  Cs.resize(batch_count);
+  Ds.resize(batch_count);
+
+  // Fill data
+  for (int batch = 0; batch < batch_count; batch++)
+  {
+
+    As[batch].resize(N);
+    Aptrs[batch] = As[batch].device_data();
+
+    Bs[batch].resize(M_b, N_b);
+    Bptrs[batch] = Bs[batch].device_data();
+
+    Cs[batch].resize(M);
+    Cptrs[batch] = Cs[batch].device_data();
+
+    Ds[batch].resize(M);
+    Dptrs[batch] = Ds[batch].data();
+
+    for (int i = 0; i < N; i++)
+      As[batch][i] = i;
+
+    for (int j = 0; j < M_b; j++)
+      for (int i = 0; i < N_b; i++)
+        Bs[batch][j][i] = i + j * 2;
+
+    for (int i = 0; i < M; i++)
+      Cs[batch][i] = Ds[batch][i] = T(0);
+
+    As[batch].updateTo();
+    Bs[batch].updateTo();
+    //Skil updateTo on C as beta=0
+    //Cs[batch].updateTo();
+  }
+
+  T alpha=T(1);
+  T beta=T(0);
+
+  const auto transA = (trans == 'T') ? oneapi::mkl::transpose::trans: oneapi::mkl::transpose::nontrans;
+
+  syclBLAS::gemv_batched(*handle, transA, N_b, M_b, 
+      &alpha, Bptrs.data(), N_b, Aptrs.data(), 1, &beta, Cptrs.data(), 1, batch_count).wait();
+
+  for (int batch = 0; batch < batch_count; batch++)
+  {
+    Cs[batch].updateFrom();
+    if (trans == 'T')
+      BLAS::gemv_trans(M_b, N_b, Bs[batch].data(), As[batch].data(), Ds[batch].data());
+    else
+      BLAS::gemv(M_b, N_b, Bs[batch].data(), As[batch].data(), Ds[batch].data());
+
+    // Check results
+    for (int index = 0; index < M; index++)
+      CHECK(Cs[batch][index] == Ds[batch][index]);
+  }
+}
+
 TEST_CASE("OmpSYCL gemv", "[SYCL]")
 {
   const int M           = 137;
@@ -192,10 +285,13 @@ TEST_CASE("OmpSYCL gemv", "[SYCL]")
   test_gemv<std::complex<double>,OMPallocator<std::complex<double>>>(N, M, 'T');
 #endif
 
-  // batched
-  std::cout << "Testing TRANS gemv_batched" << std::endl;
+  std::cout << "Testing TRANS gemv_batched using OMPallocator" << std::endl;
   test_gemv_batched<float>(M, N, 'T', batch_count);
   test_gemv_batched<double>(M, N, 'T', batch_count);
+
+  std::cout << "Testing TRANS gemv_batched SYCLHostAllocator" << std::endl;
+  test_gemv_batched_pinned<float>(M, N, 'T', batch_count);
+  test_gemv_batched_pinned<double>(M, N, 'T', batch_count);
 #if defined(QMC_COMPLEX)
   test_gemv_batched<std::complex<float>>(M, N, 'T', batch_count);
   test_gemv_batched<std::complex<double>>(M, N, 'T', batch_count);
